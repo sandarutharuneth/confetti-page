fd9460996acdcb18ec288ada6e22f2efee4bd11f
Sandaru Tharuneth <sandarutharuneth@gmail.com>
2023-01-27 16:29:23 +0530

    Update confetti.js

--- confetti.js
+++ confetti.js
@@ -3,7 +3,7 @@ var confetti = {
 	speed: 2,		//set the particle animation speed
 	frameInterval: 15,	//the confetti animation frame interval in milliseconds
 	alpha: 1.0,		//the alpha opacity of the confetti (between 0 and 1, where 1 is opaque and 0 is invisible)
-	gradient: false,	//whether to use gradients for the confetti particles
+	gradient: true,	//whether to use gradients for the confetti particles
 	start: null,		//call to start confetti animation (with optional timeout in milliseconds, and optional min and max random confetti count)
 	stop: null,		//call to stop adding confetti
 	toggle: null,		//call to start or stop the confetti animation depending on whether it's already running
